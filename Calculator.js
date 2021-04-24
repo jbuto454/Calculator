0b58dc4169db740031156eb21f85a584cb958b59
Jake <jakebuto@pm.me>
2021-04-24 17:44:49 -0400

    everything working besides the actual calculations

--- Calculator.js
+++ Calculator.js
@@ -78,56 +78,98 @@ displayScreen.textContent = initialDisplay;
 
 //changes the display when a button is pushed
 function changeDisplay(button) {
-	displayScreen.textContent = button;
+	console.log(typeof(button));
+	if (displayScreen.textContent !== "0" && typeof(button) === 'number' 
+	&& displayScreen.textContent !== "x" && displayScreen.textContent !== "="
+	&& displayScreen.textContent !== "-" && displayScreen.textContent !== "+"
+	&& displayScreen.textContent !== "/") {
+		displayScreen.textContent += button;
+	} else if (typeof(button) !== 'number') {
+		displayScreen.textContent = button;
+	} else {
+		displayScreen.textContent = button;
+	}
 }
+
+//clears the display so its back to its intial value
+function clearDisplay(button) {
+	displayScreen.textContent = initialDisplay;
+}
+
+//creates a empty array to store values in to operate on
+let currentInputs = [0];
+
+//store inputs into the curentInputs array
+function addToArray(input) {
+	console.log(input);
+	console.log(typeof(input));
+	console.log(displayScreen.textContent);
+	if (typeof input == 'number' && displayScreen.textContent !== "x" 
+	&& displayScreen.textContent !== "="
+	&& displayScreen.textContent !== "-" && displayScreen.textContent !== "+"
+	&& displayScreen.textContent !== "/") {
+		currentInputs[currentInputs.length - 1] = displayScreen.textContent + input;
+		console.log(currentInputs);
+	} else {
+		currentInputs.push(input);
+		console.log(currentInputs);
+	}
+}
+
+//clears the array back to only containing 0
+function clearArray(input) {
+	currentInputs = [0];
+	console.log(currentInputs);
+}
+
 
 //add event listeners that change the display on the calculator 
 //for each keyboard button
 const btn1 = document.querySelector("#btn1");
-btn1.addEventListener('click', function() {changeDisplay("1");});
+btn1.addEventListener('click', function() {addToArray(1);changeDisplay(1)});
 
 const btn2 = document.querySelector("#btn2");
-btn2.addEventListener('click', function() {changeDisplay("2");});
+btn2.addEventListener('click', function() {addToArray(2);changeDisplay(2);});
 
 const btn3 = document.querySelector("#btn3");
-btn3.addEventListener('click', function() {changeDisplay("3");});
+btn3.addEventListener('click', function() {addToArray(3);changeDisplay(3);});
 
 const btn4 = document.querySelector("#btn4");
-btn4.addEventListener('click', function() {changeDisplay("4");});
+btn4.addEventListener('click', function() {addToArray(4);changeDisplay(4);});
 
 const btn5 = document.querySelector("#btn5");
-btn5.addEventListener('click', function() {changeDisplay("5");});
+btn5.addEventListener('click', function() {addToArray(5);changeDisplay(5);});
 
 const btn6 = document.querySelector("#btn6");
-btn6.addEventListener('click', function() {changeDisplay("6");});
+btn6.addEventListener('click', function() {addToArray(6);changeDisplay(6);});
 
 const btn7 = document.querySelector("#btn7");
-btn7.addEventListener('click', function() {changeDisplay("7");});
+btn7.addEventListener('click', function() {addToArray(7);changeDisplay(7);});
 
 const btn8 = document.querySelector("#btn8");
-btn8.addEventListener('click', function() {changeDisplay("8");});
+btn8.addEventListener('click', function() {addToArray(8);changeDisplay(8);});
 
 const btn9 = document.querySelector("#btn9");
-btn9.addEventListener('click', function() {changeDisplay("9");});
+btn9.addEventListener('click', function() {addToArray(9);changeDisplay(9);});
 
 const btn0 = document.querySelector("#btn0");
-btn0.addEventListener('click', function() {changeDisplay("0");});
+btn0.addEventListener('click', function() {addToArray(0);changeDisplay(0);});
 
 const Equal = document.querySelector("#Equal");
-Equal.addEventListener('click', function() {changeDisplay("=");});
+Equal.addEventListener('click', function() {addToArray("=");changeDisplay("=");});
 
 const clear = document.querySelector("#clear");
-clear.addEventListener('click', function() {changeDisplay("0");});
+clear.addEventListener('click', function() {clearArray();clearDisplay();});
 
 const Subtract = document.querySelector("#Subtract");
-Subtract.addEventListener('click', function() {changeDisplay("-");});
+Subtract.addEventListener('click', function() {addToArray("-");changeDisplay("-");});
 
 const Add = document.querySelector("#Add");
-Add.addEventListener('click', function() {changeDisplay("+");});
+Add.addEventListener('click', function() {addToArray("+");changeDisplay("+");});
 
 const Divide = document.querySelector("#Divide");
-Divide.addEventListener('click', function() {changeDisplay("/");});
+Divide.addEventListener('click', function() {addToArray("/");changeDisplay("/");});
 
 const Multiply = document.querySelector("#Multiply");
-Multiply.addEventListener('click', function() {changeDisplay("x");});
+Multiply.addEventListener('click', function() {addToArray("x");changeDisplay("x");});
 
